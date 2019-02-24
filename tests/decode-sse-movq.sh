dfd70eef39f61d706705b238c7aacc8ec3e58c2c
Alexis Engelke <alexis.engelke@googlemail.com>
2019-02-24 10:09:18 +0100

    Fix bug with VMOV[DQ] operand width in 32-bit mode

--- tests/decode-sse-movq.sh
+++ tests/decode-sse-movq.sh
@@ -1,1 +1,4 @@
 decode f30f7e5c2408 [SSE_MOVQ_X2X reg8:r3 mem8:r4+0x8]
+decode c5f96ec8 [VMOVD_G2X reg4:r1 reg4:r0]
+decode64 c4e1f96ec8 [VMOVQ_G2X reg8:r1 reg8:r0]
+decode32 c4e1f96ec8 [VMOVD_G2X reg4:r1 reg4:r0]
