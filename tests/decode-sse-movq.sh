bb4b195dbe6414bf5b194e75dbc7cc18eed95d04
Alexis Engelke <alexis.engelke@googlemail.com>
2020-06-18 19:07:09 +0200

    instrs/sse,avx: Fix several operand sizes

--- tests/decode-sse-movq.sh
+++ tests/decode-sse-movq.sh
@@ -1,4 +1,4 @@
-decode f30f7e5c2408 [SSE_MOVQ reg8:r3 mem8:r4+0x8]
+decode f30f7e5c2408 [SSE_MOVQ reg16:r3 mem8:r4+0x8]
 decode c5f96ec8 [VMOVD reg4:r1 reg4:r0]
 decode64 c4e1f96ec8 [VMOVQ reg8:r1 reg8:r0]
 decode32 c4e1f96ec8 [VMOVD reg4:r1 reg4:r0]
