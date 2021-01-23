103fc536b082b384d7fcb31b405f6bb0d04da135
Alexis Engelke <alexis.engelke@googlemail.com>
2021-01-23 14:39:48 +0100

    instrs: Fix operands of VRCPPS/VSQRTP[SD]/VRSQRTPS

--- tests/test_decode.c
+++ tests/test_decode.c
@@ -418,6 +418,7 @@ main(int argc, char** argv)
     TEST("\xc5\xf2\x2a\xc0", "vcvtsi2ss xmm0, xmm1, eax");
     TEST32("\xc4\xe1\xf2\x2a\xc0", "vcvtsi2ss xmm0, xmm1, eax");
     TEST64("\xc4\xe1\xf2\x2a\xc0", "vcvtsi2ss xmm0, xmm1, rax");
+    TEST("\xc5\xf8\x53\xc0", "vrcpps xmm0, xmm0");
 
     TEST("\xc4\xe3\x79\x14\xc0\x00", "vpextrb eax, xmm0, 0x0");
     TEST("\xc4\xe3\xf9\x14\xc0\x00", "vpextrb eax, xmm0, 0x0");
