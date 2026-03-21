ec4f2c90cdbe6962a32e93ea60783004d31da0f1
Alexis Engelke <alexis.engelke@googlemail.com>
2026-03-21 14:39:56 +0100

    encode-test: Store test cases as data

--- encode-test.c
+++ encode-test.c
@@ -6,6 +6,14 @@
 
 #include <fadec-enc.h>
 
+#if defined(__has_attribute)
+#if __has_attribute(nonstring)
+#define NONSTRING __attribute__((nonstring))
+#endif
+#endif
+#if !defined(NONSTRING)
+#define NONSTRING
+#endif
 
 static
 void
@@ -15,47 +23,49 @@ print_hex(const uint8_t* buf, size_t len)
         printf("%02x", buf[i]);
 }
 
-static
-int
-test(uint8_t* buf, const char* name, uint64_t mnem, uint64_t op0, uint64_t op1, uint64_t op2, uint64_t op3, const void* exp, size_t exp_len)
-{
-    memset(buf, 0, 16);
-
-    uint8_t* inst = buf;
-    int res = fe_enc64(&inst, mnem, op0, op1, op2, op3);
-    if ((res != 0) != (exp_len == 0)) goto fail;
-    if (inst - buf != (ptrdiff_t) exp_len) goto fail;
-    if (memcmp(buf, exp, exp_len)) goto fail;
-
-    return 0;
-
-fail:
-    printf("Failed case %s:\n", name);
-    printf("  Exp (%2zu): ", exp_len);
-    print_hex(exp, exp_len);
-    printf("\n  Got (%2zd): ", inst - buf);
-    print_hex(buf, inst - buf);
-    printf("\n");
-    return -1;
-}
-
-#define TEST2(str, exp, exp_len, mnem, flags, op0, op1, op2, op3, ...) test(buf, str, FE_ ## mnem|flags, op0, op1, op2, op3, exp, exp_len)
-#define TEST1(str, exp, ...) TEST2(str, exp, sizeof(exp)-1, __VA_ARGS__, 0, 0, 0, 0, 0)
-#define TEST(exp, ...) failed |= TEST1(#__VA_ARGS__, exp, __VA_ARGS__);
-
 int
 main(int argc, char** argv)
 {
     (void) argc; (void) argv;
 
-    int failed = 0;
-    uint8_t buf[16];
-
+    static uint8_t buf[16];
+    static const struct TestCase {
+        const char *name;
+        uint64_t mnem;
+        uint8_t exp_len;
+        uint8_t exp[15] NONSTRING;
+        FeOp op0, op1, op2, op3;
+    } cases[] = {
     // VSIB encoding doesn't differ for this API
 #define FE_MEMV FE_MEM
 #define FE_PTR(off) ((intptr_t) buf + (off))
 #define FLAGMASK(flags, mask) (flags | FE_MASK(mask & 7))
+#define TEST1(str, exp, exp_len, mnem, flags, op0, op1, op2, op3, ...) \
+        {str, FE_##mnem|flags, exp_len, exp, op0, op1, op2, op3},
+#define TEST(exp, ...) TEST1(#__VA_ARGS__, exp, sizeof(exp)-1, __VA_ARGS__, 0, 0, 0, 0,)
 #include "encode-test.inc"
+    };
+
+    int failed = 0;
+
+    const struct TestCase* end = cases + sizeof(cases) / sizeof(cases[0]);
+    for (const struct TestCase* tc = cases; tc != end; tc++) {
+        memset(buf, 0, 16);
+
+        uint8_t* inst = buf;
+        int res = fe_enc64(&inst, tc->mnem, tc->op0, tc->op1, tc->op2, tc->op3);
+        if ((res != 0) != (tc->exp_len == 0) ||
+            inst - buf != (ptrdiff_t) tc->exp_len ||
+            memcmp(buf, tc->exp, tc->exp_len)) {
+            failed += 1;
+            printf("Failed case %s:\n", tc->name);
+            printf("  Exp (%2u): ", tc->exp_len);
+            print_hex(tc->exp, tc->exp_len);
+            printf("\n  Got (%2zd): ", inst - buf);
+            print_hex(buf, inst - buf);
+            printf("\n");
+        }
+    }
 
     puts(failed ? "Some tests FAILED" : "All tests PASSED");
     return failed ? EXIT_FAILURE : EXIT_SUCCESS;
